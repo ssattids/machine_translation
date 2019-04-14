239c178d574159650a6702a52662148e62517192
Carlos Mondragon <cmondragonch@gypsum.cm.cluster>
2019-04-14 14:24:37 -0400

    Working version of run.sh

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@
 #SBATCH --job-name=nlp
 #SBATCH --output=res_%j.txt  # output file
 #SBATCH -e res_%j.err        # File to which STDERR will be written
-#SBATCH --partition=m40-long # Partition to submit to 
+#SBATCH --partition=titanx-short # Partition to submit to 
 #
 #SBATCH --ntasks=1
-#SBATCH --time=10:00:00         # Runtime in D-HH:MM
+#SBATCH --time=04:00:00         # Runtime in D-HH:MM
 #SBATCH --mem-per-cpu=6000    # Memory in MB per cpu allocated
 
 python nlp_final_project_v2.py
